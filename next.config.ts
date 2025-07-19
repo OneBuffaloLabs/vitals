72451866c850b9cc7db93bf273c2165744edbfc8
Bana0516 <aelbaneh0615@gmail.com>
2025-07-19 02:08:25 -0400

    [Issue-2] Integrate Tailwind CSS

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  output: 'export',
+  trailingSlash: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production', // Only remove in production
+  },
 };
 
 export default nextConfig;
