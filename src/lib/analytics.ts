68f1b9862f991ca3387d7144f99c87d675936580
Bana0516 <aelbaneh0615@gmail.com>
2025-08-18 15:27:27 -0400

    Updated the sitemap to strip the trailing slashes on images

--- src/lib/analytics.ts
+++ src/lib/analytics.ts
@@ -3,21 +3,22 @@ import ReactGA from 'react-ga4';
 const GA_MEASUREMENT_ID = 'G-05JYVKSB5Q';
 
 export const initGA = (): void => {
-  // Check for production environment
-  if (process.env.NODE_ENV === 'production') {
+  // Check for production environment and if the GA ID exists
+  if (GA_MEASUREMENT_ID && process.env.NODE_ENV === 'production') {
     ReactGA.initialize(GA_MEASUREMENT_ID);
   }
 };
 
 export const logPageView = (): void => {
-  if (process.env.NODE_ENV === 'production') {
+  // Check for production environment and if the GA ID exists
+  if (GA_MEASUREMENT_ID && process.env.NODE_ENV === 'production') {
     ReactGA.send({ hitType: 'pageview', page: window.location.pathname });
   }
 };
 
-// Note the explicit types for the event parameters
 export const logEvent = (category: string, action: string, label?: string): void => {
-  if (process.env.NODE_ENV === 'production') {
+  // Check for production environment and if the GA ID exists
+  if (GA_MEASUREMENT_ID && process.env.NODE_ENV === 'production') {
     ReactGA.event({ category, action, label });
   }
 };
